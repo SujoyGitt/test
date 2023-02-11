54525a255306dcfb473644dacf2553b13d043a7f
Sujoy Ghosal <sujoy143656@gmail.com>
2023-02-11 10:34:30 +0530

    this is login form

--- java.js
+++ java.js
@@ -1,19 +1,28 @@
-let eye = document.querySelector('.fa-eye');
+let eye = document.querySelector('.openeye');
+
+let password = document.querySelector("#pass");
+let user = document.getElementById('user');
+let pattern = /[a-z]@gmail.com$/;
+let passpattern = /([A-Z0-9a-z.@<>%$#&*(){}]){1,5}([a-z0-9]){1}([A-Z.@<>%$#&*(){}]){1}([0-9a-z]){1}/;
+let form = document.querySelector("form");
+let btn =  document.querySelector("button");
+
 eye.addEventListener("click",function () {
     let e = document.querySelector('#pass').getAttribute('type');
-    
     if (e == "password") {
         document.querySelector('#pass').setAttribute('type','text');
+        eye.classList.remove('fa-eye');
+        eye.classList.add('fa-eye-slash');
     }
     else{
         document.querySelector('#pass').setAttribute('type','password'); 
+        eye.classList.add('fa-eye');
+        eye.classList.remove('fa-eye-slash');
     }
 });
-let user = document.getElementById('user');
-user.addEventListener("keyup",function () {
+ user.addEventListener("keyup",function () {
     let email = document.getElementById('user').value;
-    let text = document.getElementById('text');  
-    let pattern = /[a-z]@gmail.com$/;
+    let text = document.getElementById('text')
     if (email.match(pattern)) {
         text.innerHTML = "Your Email Address in Valid.";
         text.style.color = 'lime';
@@ -24,11 +33,11 @@ user.addEventListener("keyup",function () {
         text.style.color = 'red';
         text.style.fontSize = '14px';
     }
+    
 });
 document.getElementById("pass").addEventListener("keyup",()=>{
     let password = document.getElementById('pass').value;
     let passtext = document.getElementById('notipass');
-    let passpattern = /([A-Z0-9a-z.@<>%$#&*(){}]){1,5}([a-z0-9]){1}([A-Z.@<>%$#&*(){}]){1}([0-9a-z]){1}/;
     if (password.match(passpattern) && password.length >= 8) {
         passtext.innerHTML = "your password match";
         passtext.style.color = 'aqua';
@@ -40,4 +49,20 @@ document.getElementById("pass").addEventListener("keyup",()=>{
         passtext.style.fontSize = '12px';
     };
 
-})
+});
+let fom =document.querySelector("form");
+
+fom.addEventListener("keyup",()=>{
+    if(user.value.length > 10 && password.value.length > 7){
+     btn.removeEventListener('mouseover',hover);
+     btn.classList.remove('btnadd');
+    }
+});
+
+let hover = ()=>{
+    btn.classList.toggle('btnadd');
+}  
+btn.addEventListener("mouseover",hover);
+
+ 
+
